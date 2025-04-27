06c433b812dcb90e8bd9bc4d8ffd8140abe3d977
galvaolucas <lucasmelo_galvao@hotmail.com>
2025-04-27 03:58:07 +0000

    Deploying to main from @ galvaolucas/portfolio@9528e57ca5d0625f55172ec58a2c14020647a813 🚀

--- src/components/ui/Layout.tsx
+++
@@ -1,7 +0,0 @@
-export const Layout = ({ children }: { children: React.ReactNode }): React.ReactElement => {
-  return (
-    <div className="w-screen min-h-screen bg-ice dark:bg-black">
-      {children}
-    </div>
-  )
-}
