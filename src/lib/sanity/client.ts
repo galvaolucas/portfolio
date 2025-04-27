c610c7f31daf21d2e736fe1417c754e6539f1d05
galvaolucas <lucasmelo_galvao@hotmail.com>
2025-04-27 04:06:27 +0000

    Deploying to gh-pages from @ galvaolucas/portfolio@be581e5dbb03fb77e9e6ef1758e3683abcf90d81 🚀

--- src/lib/sanity/client.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@sanity/client";
-
-export const client = createClient({
-  projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
-  dataset: "production",
-  apiVersion: "2025-04-26",
-  useCdn: false,
-});
