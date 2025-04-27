6eac1266142065fceec25998f47deee63c1c4b5c
Lucas Galvao <lucas.galvao@instacasa.com.br>
2025-04-27 00:03:05 -0300

    fixing build

--- src/components/ui/Layout.tsx
+++ src/components/ui/Layout.tsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 export const Layout = ({ children }: { children: React.ReactNode }): React.ReactElement => {
   return (
     <div className="w-screen min-h-screen bg-ice dark:bg-black">
