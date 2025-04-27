bc78a96eb402194f17784f52e81cea0537cc6dcd
Lucas Galvao <lucas.galvao@instacasa.com.br>
2025-04-27 01:29:39 -0300

    attempt 2

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,22 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from 'path'
+import { writeFileSync } from 'fs'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    {
+          name: 'copy-index-to-404',
+          closeBundle: () => {
+            const indexPath = resolve(__dirname, 'dist/index.html');
+            const notFoundPath = resolve(__dirname, 'dist/404.html');
+            writeFileSync(notFoundPath, require('fs').readFileSync(indexPath));
+          }
+        }
+  ],
   server: {
     cors: true,
   },
